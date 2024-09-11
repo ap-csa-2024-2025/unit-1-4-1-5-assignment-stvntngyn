1dd223bef196adca240c6dc06b973772086ae443
Steven Nguyen <snguyen155@student.hbuhsd.edu>
2024-09-11 10:53:48 -0700

    finished

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
-
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    int val = 867;
+    int num1 = (val % 10); 
+    val /= 10;
+    int num2 = (val % 10);
+    val /= 10; 
+    int num3 = (val % 10); 
+    System.out.println(num3);
+    System.out.println(num2);
+    System.out.println(num1);
+  }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,15 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    int val = 8672;
+    System.out.println(val % 10); 
+    val /= 10;
+    System.out.println(val % 10);
+    val /= 10; 
+    System.out.println(val % 10);
+    val /= 10; 
+    System.out.println(val % 10);  
+  }
+}
 
